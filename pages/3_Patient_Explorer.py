040c6c2aa732c6f21c0250961979e1ef7e6d9715
VoidRay-X <anmolkm18@gmail.com>
2026-02-24 15:01:44 +0530

    Update 3_Patient_Explorer.py

--- pages/3_Patient_Explorer.py
+++ pages/3_Patient_Explorer.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from data_loader import load_data
+from utils import sidebar_filters
 
+st.title("🧑‍⚕️ Patient Explorer")
+
+df = load_data()
+filtered_df = sidebar_filters(df)
+
+patient_id = st.selectbox(
+    "Select Patient",
+    filtered_df["patient_id"].unique()
+)
+
+patient_data = filtered_df[filtered_df["patient_id"] == patient_id]
+
+st.write("Patient Details")
+st.dataframe(patient_data)
