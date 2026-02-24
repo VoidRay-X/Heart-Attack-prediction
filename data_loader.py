f4a22ba50b6e2c15c5c3b4d928e23cf303e8b4e2
VoidRay-X <anmolkm18@gmail.com>
2026-02-24 14:58:05 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import streamlit as st
 
+@st.cache_data
+def load_data():
+    # If CSV files
+    patients = pd.read_csv("clean_patients.csv")
+    heart = pd.read_csv("clean_heart_records.csv")
+
+    # Merge on patient_id
+    df = patients.merge(heart, on="patient_id", how="inner")
+
+    return df
