a0d5a1839f27c3985468438bb2dc70766f9f6f7f
VoidRay-X <anmolkm18@gmail.com>
2026-05-29 19:33:44 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,8 +4,8 @@ import streamlit as st
 @st.cache_data
 def load_data():
     # If CSV files
-    patients = pd.read_csv("clean_patients.csv")
-    heart = pd.read_csv("clean_heart_records.csv")
+    patients = pd.read_csv("Data/clean_patients.csv")
+    heart = pd.read_csv("Data/clean_heart_records.csv")
 
     # Merge on patient_id
     df = patients.merge(heart, on="patient_id", how="inner")
